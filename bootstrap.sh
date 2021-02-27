15a4269ce4351f06a9126c89853e18edecb56867
Victor Nystad <vnys@equinor.com>
2021-02-27 21:48:59 +0000

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/fish
 
-export DEBIAN_FRONTEND=noninteractive
+function install-linux-packages
+  echo "Installing Linux stuff…"
+  set -x DEBIAN_FRONTEND noninteractive
+  sudo apt-get update
+  sudo apt-get -y install build-essential
+end
 
-sudo apt-get update
-sudo apt-get -y install --no-install-recommends apt-utils dialog
-sudo apt-get install -y \
-  exa
+function install-homebrew
+  echo "Installing Homebrew…"
+  wget -qO- https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
+  /home/linuxbrew/.linuxbrew/bin/brew shellenv >> $HOME/.config/fish/config.fish
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+  brew install exa
+end
+
+install-linux-packages
+install-homebrew
 
 # Install Jetbrains Mono font
-wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
-sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
-sudo fc-cache -f -v
+# wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
+# sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
+# sudo fc-cache -f -v
